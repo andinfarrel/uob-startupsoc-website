0ccac15d67f3433f54fe5590619f7d69e54fb752
Vilmos Czeroczky <v.czeroczky@gmail.com>
2019-10-08 08:10:51 +0100

    Created node app

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express    = require('express'),
+      app        = express(),
+      bodyParser = require('body-parser'),
+      path       = require('path'),
+      server     = require('http').createServer(app),
+      https      = require('https');
+
+// const logging = require('./logging');
+
+const port          = process.env.PORT,
+      serverAddress = process.env.ADDRESS;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, '/views'));
+
+app.get("/", (req, res) => {
+    res.send('This is my website!');
+});
+
+server.listen(port, serverAddress, () => {
+    console.log(`Server listening at ${serverAddress}:${port}`);
+});
