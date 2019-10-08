8d24924a3d6e4573c72df2dcac28cc8400906268
Vilmos Czeroczky <v.czeroczky@gmail.com>
2019-10-08 15:56:27 +0100

    Corrected public path

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port          = process.env.PORT,
       serverAddress = process.env.ADDRESS;
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', path.join(__dirname, '/views'));
 
